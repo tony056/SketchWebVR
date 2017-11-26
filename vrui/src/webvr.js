88aa62e3d3d91e705af26794782cdc630a2bb17a
Tony Tung <tony61507@gmail.com>
2017-11-26 13:50:12 -0800

    generate basic html content

--- vrui/src/webvr.js
+++ vrui/src/webvr.js
@@ -2,7 +2,7 @@ let HTML_HEAD = '<!DOCTYPE html>\n\
 <html lang="en">\n\
 <head>\n\
 <meta charset="utf-8">\n\
-<title>' + [doc displayName] + '</title>\n\
+<title>' + 'webVR' + '</title>\n\
 <meta name="viewport" content="width=device-width, initial-scale=1.0">\n\
 <script src="https://aframe.io/releases/0.7.0/aframe.min.js"></script> \n\
 </head>\n\
@@ -17,3 +17,32 @@ let HTML_END = '<a-entity camera look-controls>\n\
 </a-scene>\n\
 </body>\n\
 </html>\n';
+
+
+
+var createContent = (groups) => {
+    const assets = createAssets(groups);
+    const uis = createUIs(groups);
+    return `${HTML_HEAD}${assets}${uis}${HTML_END}`;
+};
+
+var createAssets = (groups) => {
+    var assets = '';
+    for(let group of groups){
+        let name = group.name;
+        assets += `<img id="${name}" src="./img/${name}.png">\n`;
+    }
+    return `<a-assets>\n${assets}</a-assets>\n`;
+};
+
+var createUIs = (groups) => {
+    var uis = '';
+    for(let group of groups){
+        let name = group.name;
+        uis += `<a-image src="#${name}"></a-image>\n`;
+    }
+    return uis;
+}
+
+
+export {createContent};
