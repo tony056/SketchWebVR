874268e58d8afc33520c0fcaeae84d07b06eff6f
Tony Tung <tony61507@gmail.com>
2017-11-26 11:06:53 -0800

    file folder functions

---
+++ vrui/src/utils.js
@@ -0,0 +1,27 @@
+
+
+var getDMMScale = (distance) => {
+    return distance;
+}
+
+var getCurrentPath = (docObj) => {
+    return docObj.fileURL().path().split(docObj.displayName())[0];
+}
+
+var createFolder = (path, name) => {
+    deleteFolder(path, name);
+    var fileManager = NSFileManager.defaultManager();
+    fileManager.createDirectoryAtPath_withIntermediate_attributes_error_(path + '/' + name, true, nil, nil);
+}
+
+var deleteFolder = (path, name) => {
+    var fileManager = NSFileManager.defaultManager();
+    fileManager.removeItemAtPath_error(path + '/' + name, nil);
+}
+
+let exportOptions = {
+    "scales": "1",
+    "formats": "png"
+};
+
+export {getDMMScale, exportOptions, getCurrentPath};
